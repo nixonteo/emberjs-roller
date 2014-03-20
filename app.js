fc38f2dc6fee641f5b91e6f1823b8a4c71e0db99
unknown <Nixon@.(none)>
2014-03-20 23:37:34 +1100

    20 March 2014 Night Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var Roller = Ember.Application.create({
+    LOG_TRANSITIONS: true,
+    LOG_BINDINGS: true,
+    LOG_VIEW_LOOKUPS: true,
+    LOG_STACKTRACE_ON_DEPRECATION: true,
+    LOG_VERSION: true,
+    debugMode: true
+});
+
+Roller.Router.map(function () { 
+    this.resource("roll");
+    this.resource("roll2" , {path: "/road-not-taken"});
+    this.resource("roll3" , {path: "/highway-to-heaven"}, function(){
+    	this.route("gate");
+    });
+    
+});
+
+//Index Route
+Roller.IndexRoute = Ember.Route.extend({
+	setupController: function(controller) {
+	    // Set the IndexController's `title`
+	    controller.set('on_index', "You are in the index page");
+	  }
+});
+
+Roller.RollRoute = Ember.Route.extend({
+	setupController: function(controller){
+		controller.set('on_roll', "You are in the roll page");
+	}
+});
